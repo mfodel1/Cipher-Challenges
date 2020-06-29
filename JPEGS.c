f06dc474334a1f946df6f2782bdbb695bf81024e
Matthew Fodel <mfodel1@umbc.edu>
2020-06-29 16:02:36 -0400

    loop conditions

--- JPEGS.c
+++ JPEGS.c
@@ -4,39 +4,44 @@
 #include "KDB-Files.c"
 #include <stdint.h>
 #include <stdlib.h>
-
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main(int argc, const char *argv[]){
-  // TO START:
-    char jpgBuf[3], endCheck[2];
+    unsigned char jpgBuf[3], endCheck[2];
     unsigned char standard[3] = {0xFF, 0xD8, 0xFF};
-    unsigned char jpgEnd[2] = {0xFF, 0xD9};
+    unsigned char jpegEnd[2] = {0xFF, 0xD9};
+    long offset;
+    int fileSize;
     // call readKdb to get magic bytes from given file.
     unsigned char *readResult = readKDB(argv[1], 1);
-    long offset;
-    int fileSize = 3; // initialized with 3 standard bytes.
-    // read 3 bytes at a time from input.bin until the magic starting bytes are found.
-    // find every JPEG file that starts with the custom magic bytes found in the KDB file
+
+    char *dirName = strcat(argv[2], "_Repaired");
+    mkdir(dirName);
+    strcat(dirName, "/");
+
     FILE *fp = fopen(argv[2], "rb+");
-    while(fp != EOF){
-      fread(&jpgBuf, 3, 1, fp);
-      if (jpgBuf == readResult){ // magic starting bits are found.
-        // save the offset where the byte pattern was found, the size of the file
-        offset = ftell(fp); // final offset will be offset - 3 since fread advances fp
-        // replace the custom bits with the standard bits.
-        fwrite(standard, 1, sizeof(standard), fp);
-        // count the number of bytes in the file and write each word from input into a new output file.
+    while(){ // figure out the loop condition
+      fgets(jpgBuf, 4, fp);
+      if (jpgBuf == readResult){
+        // save the offset where the byte pattern was found
+        offset = ftell(fp);
+        offset -= 3;
+        char *fileName = strcat((char)offset, ".jpeg");
+        char *filePath = strcat(dirName, fileName);
+        FILE *fp1 = fopen(filePath, "w");
+        fileSize += fwrite(standard, 1, sizeof(standard), fp1);
         fread(&endCheck, 2, 1, fp);
-        while (endCheck != jpgEnd) {
-            size += fwrite();
+        while (endCheck != jpegEnd) {
+            fileSize += fwrite(endCheck, 1, 2, fp1);
+            fread(&endCheck, 2, 1, fp);
           }
+        fileSize += fwrite(endCheck, 1, 2, fp1);
+        printf("File finished writing\n");
+        // printf the file offset, fileSize, and the file's MD5 hash, and the filePath.
+        fclose(fp1);
         }
-
       }
-    }
     fclose(fp);
-
-  // TO FINISH:
-    // figure out the MD5 hash and what else to output.
     return 0;
 }
