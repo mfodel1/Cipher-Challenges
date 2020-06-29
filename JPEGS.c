a8013485367bce40b249a033747e62a3a58c0d7e
Matthew Fodel <mfodel1@umbc.edu>
2020-06-29 13:20:06 -0400

    more planning, implementation for c3

--- JPEGS.c
+++ JPEGS.c
@@ -8,20 +8,30 @@
 
 int main(int argc, const char *argv[]){
   // TO START:
-    char magicStart[3];
+    char jpgBuf[3], endCheck[2];
     unsigned char standard[3] = {0xFF, 0xD8, 0xFF};
-    // call readKdb to get magic bytes.
+    unsigned char jpgEnd[2] = {0xFF, 0xD9};
+    // call readKdb to get magic bytes from given file.
     unsigned char *readResult = readKDB(argv[1], 1);
-
+    long offset;
+    int fileSize = 3; // initialized with 3 standard bytes.
     // read 3 bytes at a time from input.bin until the magic starting bytes are found.
     // find every JPEG file that starts with the custom magic bytes found in the KDB file
-    FILE *fp = fopen(argv[2], "rb");
+    FILE *fp = fopen(argv[2], "rb+");
     while(fp != EOF){
-      fread(&magicStart, 3, 1, fp);
-      if (magicStart == readResult){ // magic starting bits are found.
-        // save the offset where the byte pattern was found, the size of the file.
+      fread(&jpgBuf, 3, 1, fp);
+      if (jpgBuf == readResult){ // magic starting bits are found.
+        // save the offset where the byte pattern was found, the size of the file
+        offset = ftell(fp); // final offset will be offset - 3 since fread advances fp
         // replace the custom bits with the standard bits.
         fwrite(standard, 1, sizeof(standard), fp);
+        // count the number of bytes in the file and write each word from input into a new output file.
+        fread(&endCheck, 2, 1, fp);
+        while (endCheck != jpgEnd) {
+            size += fwrite();
+          }
+        }
+
       }
     }
     fclose(fp);
